3ff9b37bf72393e233273dd4d1f9c2c13762dec1
Stefan <satalbot@protonmail.com>
2022-09-25 16:01:31 +0000

    Added 'Energy' to keywords, and commented out redundant keywords

--- src/data/keywords.js
+++ src/data/keywords.js
@@ -1,18 +1,26 @@
 //List of keywords
 module.exports = keywords = [
-  'green energy',
-  'energy price',
-  'energy prices',
-  'energy firms',
-  'renewable energy',
-  'energy company',
-  'energy tax',
-  'energy bill', // new
-  'energy resources', // new
+  //'green energy',
+  //'energy price',
+  //'energy prices',
+  //'energy firms',
+  //'renewable energy',
+  //'energy company',
+  //'energy tax',
+  //'energy bill', // new
+  //'energy resources', // new
+  //'Energy price',
+  //'Energy prices',
+  //'Energy firms',
+  //'Energy company',
+  //'Energy tax',
+  //'Energy bill', // new
+  //'Energy resources', // new
+  'energy', // new
   'electricity bill', // new
   'gas bill', // new
   'electricity costs', // new
-  'energy', // new
+  'Energy',
   'gas prices', //new
   'petrol prices', //new
 ];
